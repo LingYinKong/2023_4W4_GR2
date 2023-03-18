b1e76e404a462f21ea442fe30f79f20958e188bc
LingYinKong <112096497+LingYinKong@users.noreply.github.com>
2023-03-17 22:38:46 -0400

    s8c2 Ajout des liens dans le footer et modification scss du pied de page

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <footer class="site__footer">
-    <section>Colonne 1</section>
-    <section>Colonne 2</section>
-    <section>Colonne 3</section>
+    <section>
+        <?php the_custom_logo(); ?>
+    </section>
+    <section>
+        <a target="_blank" href="https://github.com/LingYinKong" class="lien">Github</a>
+        <a target="_blank" href="https://cidweb9.sg-host.com/" class="lien">SiteGround</a>
+    </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
