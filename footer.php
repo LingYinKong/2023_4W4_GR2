c975940edd1513a126b750ba4fa3dc9bf7e18f54
LingYinKong <112096497+LingYinKong@users.noreply.github.com>
2023-03-17 23:13:50 -0400

    s8c2 modification footer

--- footer.php
+++ footer.php
@@ -1,13 +1,7 @@
 <footer class="site__footer">
-    <section>
-        <?php the_custom_logo(); ?>
-    </section>
-    <section>
-        <a target="_blank" href="https://github.com/LingYinKong" class="lien">Github</a>
-    </section>
-    <section>
-        <a target="_blank" href="https://cidweb9.sg-host.com/" class="lien">SiteGround</a>
-    </section>
+    <a><?php the_custom_logo(); ?></a>
+    <a target="_blank" href="https://github.com/LingYinKong" class="lien">Github</a>
+    <a target="_blank" href="https://cidweb9.sg-host.com/" class="lien">SiteGround</a>
 </footer>
 <?php wp_footer(); ?>
 </body>
