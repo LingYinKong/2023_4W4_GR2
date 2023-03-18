28bb0a9d3d1ffec5392dfa9518ca025e61fbb488
LingYinKong <112096497+LingYinKong@users.noreply.github.com>
2023-03-17 22:57:19 -0400

    s8c2 Modification scss footer

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
     </section>
     <section>
         <a target="_blank" href="https://github.com/LingYinKong" class="lien">Github</a>
+    </section>
+    <section>
         <a target="_blank" href="https://cidweb9.sg-host.com/" class="lien">SiteGround</a>
     </section>
 </footer>
