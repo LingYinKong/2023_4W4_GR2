08abf339e3aec7d597d053cc526a19bcc09e18ae
LingYinKong <112096497+LingYinKong@users.noreply.github.com>
2023-03-31 09:02:00 -0400

    s9c2 Création du template-parts search-4w4.php et search-cours.php

--- search.php
+++ search.php
@@ -12,7 +12,7 @@
     if (have_posts()): 
         while (have_posts()) : the_post();
             the_title('<h4>','</h4>');?>
-     
+            
             <?= wp_trim_words(get_the_excerpt(), 50, " [...] "); ?>
             <hr>
         <?php endwhile;
